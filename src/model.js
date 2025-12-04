e2e5505beac213b0cad02692d4d30d768e7d79ec
mowwis <mo.sph@rosphere.com>
2025-12-04 15:57:01 +0100

    added client only model

---
+++ src/model.js
@@ -0,0 +1,137 @@
+export class Model extends EventTarget {
+    static fields = {};
+    static instanceCache = new Map();
+    static classEventTarget = new EventTarget();
+
+    fieldChanges = {};
+    get changeDelta() { return Object.entries(this.fieldChanges).reduce((acc, [k, v]) => { acc[k] = v.newValue; return acc; }, {}); }
+
+    constructor(data = {}) {
+        super();
+
+        this.initializeFields();
+
+        const primaryKeyValue = data[this.constructor.primaryField];
+        if (primaryKeyValue && this.constructor.instanceCache.has(primaryKeyValue)) {
+            const existingModel = this.constructor.instanceCache.get(primaryKeyValue);
+            existingModel.assign(data);
+            return existingModel;
+        }
+
+        this.assign(data);
+    }
+
+    initializeFields() { // do casting in here (all field logic / verifiaction)
+        Object.keys(this.constructor.fields).forEach(field => {
+            const privateFieldName = `_${field}`;
+            this[privateFieldName] = undefined;
+
+            Object.defineProperty(this, field, {
+                get() { return this[privateFieldName]; },
+                set(newValue) {
+                    const oldValue = this[privateFieldName];
+                    if (newValue === oldValue) return;
+                    this[privateFieldName] = newValue;
+
+                    if (this.fieldChanges[field]) this.fieldChanges[field].newValue = newValue;
+                    else this.fieldChanges[field] = { oldValue, newValue };
+                },
+                enumerable: true,
+                configurable: true
+            });
+        });
+    }
+
+    async save() {
+        let updatedData;
+        if (updatedData) this.assign(updatedData);
+    }
+
+    revert(...fields) {
+        if (!Object.keys(this.fieldChanges).length) return;
+        Object.entries(this.fieldChanges).forEach(([field, change]) => {
+            if (fields.length != 0 && !fields.includes(field)) return;
+            this[field] = change.oldValue;
+            delete this.fieldChanges[field];
+        });
+        this.dispatchEvent(ModelEvent.change(this, this.changeDelta));
+    }
+
+    async delete() {
+        if (!this.primaryKey) return;
+        this.constructor.instanceCache.delete(this.primaryKey);
+        const deleteEvent = ModelEvent.delete(this);
+        this.dispatchEvent(deleteEvent);
+        this.constructor.dispatchEvent(deleteEvent);
+    }
+
+    assign(data = {}) {
+        Object.entries(this.constructor.fields).forEach(([field, { type }]) => {
+            if (field in data) {
+                this[field] = this.constructor.castFieldValue(data[field], type);
+            }
+        });
+        const primaryKeyValue = data[this.constructor.primaryField];
+        if (primaryKeyValue && !this.constructor.instanceCache.has(primaryKeyValue)) {
+            this.constructor.instanceCache.set(primaryKeyValue, this);
+            this.constructor.dispatchEvent(ModelEvent.new(this));
+        }
+        if (Object.keys(this.fieldChanges).length) {
+            this.dispatchEvent(ModelEvent.change(this, this.changeDelta));
+        }
+        this.fieldChanges = {};
+    }
+
+    get primaryKey() { return this[this.constructor.primaryField]; }
+    static get primaryField() {
+        const primaryField = Object.keys(this.fields).find(field => this.fields[field].primary_key);
+        if (primaryField) return primaryField;
+        if ('id' in this.fields) return 'id';
+        throw new Error("Model must have a primary key or 'id' field defined.");
+    }
+
+    static dispatchEvent(event) { this.classEventTarget.dispatchEvent(event); }
+    static addEventListener(type, callback) { this.classEventTarget.addEventListener(type, callback); }
+    static removeEventListener(type, callback) { this.classEventTarget.removeEventListener(type, callback); }
+
+    static castFieldValue(value, type) {
+        if (value === null || value === undefined) return value;
+        switch (type) {
+            case Date:
+                return new Date(value);
+            case Number:
+            case String:
+            case Boolean:
+                return type(value);
+            default:
+                return value;
+        }
+    }
+
+    toJSON() {
+        const jsonObject = {};
+        Object.keys(this.constructor.fields).forEach((key) => {
+            const value = this[key];
+            if (value !== null && value !== undefined) jsonObject[key] = value instanceof Date ? value.toISOString() : value;
+        });
+        return jsonObject;
+    }
+}
+
+
+export class ModelEvent extends CustomEvent {
+    static NEW = 'new';
+    static CHANGE = 'change';
+    static DELETE = 'delete';
+
+    constructor(type, detail) {
+        const validEvents = [ModelEvent.NEW, ModelEvent.CHANGE, ModelEvent.DELETE];
+        if (!validEvents.includes(type)) throw new Error(`Invalid event type: ${type}. Valid types are ${validEvents.join(', ')}.`);
+        super(type, { detail, bubbles: true });
+    }
+
+    static new(target) { return new ModelEvent(ModelEvent.NEW, target); }
+    static delete(target) { return new ModelEvent(ModelEvent.DELETE, target); }
+    static change(target, delta) { return new ModelEvent(ModelEvent.CHANGE, { target, delta }); }
+}
+
