4eb900862a0c2966bd1b2bf6e21d7ba8af560580
mowwis <mo.sph@rosphere.com>
2025-12-13 00:04:39 +0100

    upddated distribution files

--- dist/fundle.js
+++ dist/fundle.js
@@ -1,7 +1,125 @@
+class Component extends HTMLElement {
+    bindings = {};
+    static tagName = null;
+    static template = null;
+
+    // Model
+    #model;
+    #onModelChange = () => this.render();
+    #onModelDelete = () => this.remove();
+    get model() { return this.#model; }
+    set model(modelInstance) {
+        if (this.#model) {
+            this.#model.removeEventListener(ModelEvent.CHANGE, this.#onModelChange);
+            this.#model.removeEventListener(ModelEvent.DELETE, this.#onModelDelete);
+        }
+        if (!modelInstance instanceof Model) throw new Error(`The model must be an instance of Model.`);
+        this.#model = modelInstance;
+        if (this.#model) {
+            this.#model.addEventListener(ModelEvent.CHANGE, this.#onModelChange);
+            this.#model.addEventListener(ModelEvent.DELETE, this.#onModelDelete);
+        }
+    }
+
+    // Collection
+    #collection;
+    #onCollectionChange = () => this.render();
+    get collection() { return this.#collection; }
+    set collection(collectionInstance) {
+        if (this.#collection) {
+            this.#collection.removeEventListener(ModelEvent.NEW, this.#onCollectionChange);
+            this.#collection.removeEventListener(ModelEvent.CHANGE, this.#onCollectionChange);
+        }
+        if (!collectionInstance instanceof Collection) throw new Error(`The collection must be an instance of Collection.`);
+        this.#collection = collectionInstance;
+
+        if (this.#collection) {
+            this.#collection.addEventListener(ModelEvent.NEW, this.#onCollectionChange);
+            this.#collection.addEventListener(ModelEvent.CHANGE, this.#onCollectionChange);
+        }
+    }
+
+    render() { Templater.bind(this, this.bindings); }
+
+    connectedCallback() {
+        if (this.constructor.template) {
+            this.innerHTML = "";
+            const clone = Templater.clone(this.constructor.template);
+            this.appendChild(clone);
+        }
+        this.render();
+    }
+    disconnectedCallback() {
+        this.model = null;
+        this.collection = null;
+    }
+
+    static define(tagName) {
+        this.tagName = tagName;
+        customElements.define(tagName, this);
+    }
+    static new(model, collection) {
+        if (!this.tagName) throw new Error(`Component has no tagName defined. Call .define(tagName) first.`);
+        const element = document.createElement(this.tagName);
+        if (model) element.model = model;
+        else if (collection) element.collection = collection;
+        return element;
+    }
+}
+
+class Collection extends EventTarget {
+    #items = [];
+    #onModelChange = (e) => this.dispatchEvent(ModelEvent.change(e.detail.target, e.detail.delta));
+
+    constructor(modelClass, ...initial) {
+        super();
+        this.modelClass = modelClass;
+        initial.forEach(item => this.push(item));
+        modelClass.addEventListener('new', e => this.push(e.detail)); // make use event.target
+    }
+
+    fromArray(array) {
+        array.forEach(item => this.push(item));
+        return this;
+    }
+
+    push(item) {
+        if (!(item instanceof this.modelClass)) item = new this.modelClass(item);
+        if (this.includes(item)) return;
+
+        item.addEventListener('delete', e => this.remove(item));
+        item.addEventListener('change', this.#onModelChange);
+
+        this.#items.push(item);
+        this.dispatchEvent(ModelEvent.new(item));
+    }
+
+    remove(item) {
+        const index = this.indexOf(item);
+        if (index === -1) return;
+
+        item.removeEventListener('change', this.#onModelChange);
+
+        this.splice(index, 1);
+        this.dispatchEvent(ModelEvent.delete(item));
+    }
+
+    sort(compareFn) { return this.#items.sort(compareFn); }
+    indexOf(searchElement, fromIndex) { return this.#items.indexOf(searchElement, fromIndex); }
+    includes(searchElement, fromIndex) { return this.#items.includes(searchElement, fromIndex); }
+    forEach(callbackfn) { this.#items.forEach(callbackfn); }
+
+    map(callbackfn) { return new Collection(this.modelClass).fromArray(this.#items.map(callbackfn)); }
+    filter(predicate) { return new Collection(this.modelClass).fromArray(this.#items.filter(predicate)); }
+    splice(start, deleteCount) { return new Collection(this.modelClass).fromArray(this.#items.splice(start, deleteCount)); }
+    flatMap(callback) { return new Collection(this.modelClass).fromArray(this.#items.flatMap(callback)); }
+}
+
 class Model extends EventTarget {
     static fields = {};
-    static instanceCache = new Map();
-    static classEventTarget = new EventTarget();
+    static endpoint = null;
+    static _instanceCache = new Map();
+    static _classEventTarget = new EventTarget();
 
     fieldChanges = {};
     get changeDelta() { return Object.entries(this.fieldChanges).reduce((acc, [k, v]) => { acc[k] = v.newValue; return acc; }, {}); }
@@ -9,11 +127,11 @@ class Model extends EventTarget {
     constructor(data = {}) {
         super();
 
-        this.initializeFields();
+        this.#initializeFields();
 
         const primaryKeyValue = data[this.constructor.primaryField];
-        if (primaryKeyValue && this.constructor.instanceCache.has(primaryKeyValue)) {
-            const existingModel = this.constructor.instanceCache.get(primaryKeyValue);
+        if (primaryKeyValue && this.constructor._instanceCache.has(primaryKeyValue)) {
+            const existingModel = this.constructor._instanceCache.get(primaryKeyValue);
             existingModel.assign(data);
             return existingModel;
         }
@@ -21,17 +139,21 @@ class Model extends EventTarget {
         this.assign(data);
     }
 
-    initializeFields() { // do casting in here (all field logic / verifiaction)
-        Object.keys(this.constructor.fields).forEach(field => {
-            const privateFieldName = `_${field}`;
+    #initializeFields() {
+        Object.entries(this.constructor.fields).forEach(([field, parameters]) => {
+            var type;
+            if (typeof parameters === 'object') ({ type } = parameters);
+            else type = parameters;
+
+            const privateFieldName = `#${field}`;
             this[privateFieldName] = undefined;
 
             Object.defineProperty(this, field, {
                 get() { return this[privateFieldName]; },
                 set(newValue) {
                     const oldValue = this[privateFieldName];
-                    if (newValue === oldValue) return;
-                    this[privateFieldName] = newValue;
+                    this[privateFieldName] = this.constructor._castFieldValue(newValue, type);
+                    if (this[privateFieldName] === oldValue) return;
 
                     if (this.fieldChanges[field]) this.fieldChanges[field].newValue = newValue;
                     else this.fieldChanges[field] = { oldValue, newValue };
@@ -42,8 +164,27 @@ class Model extends EventTarget {
         });
     }
 
+    static async all(params = {}) {
+        let data;
+        if (!this.endpoint) data = [...this._instanceCache.values()];
+        else {
+            var endpoint = this.endpoint;
+            Object.entries(params).forEach(([k, v]) => endpoint = endpoint.replace(`:${k}`, String(v)));
+            data = await api.get(endpoint);
+        }
+        return new Collection(this).fromArray(data);
+    }
+
+    // async fetchById(id) { ... } ??
+
     async save() {
+        if (!this.endpoint) return this.assign();
+
         let updatedData;
+        if (!this.primaryKey) updatedData = await api.post(this.endpoint, this);
+        else if (Object.keys(this.fieldChanges).length) {
+            updatedData = await api.patch(`${this.endpoint}/${this.primaryKey}`, this.changeDelta);
+        }
         if (updatedData) this.assign(updatedData);
     }
 
@@ -58,22 +199,21 @@ class Model extends EventTarget {
     }
 
     async delete() {
-        if (!this.primaryKey) return;
-        this.constructor.instanceCache.delete(this.primaryKey);
+        if (this.primaryKey) this.constructor._instanceCache.delete(this.primaryKey);
+        if (this.primaryKey && this.endpoint) await api.delete(`${this.endpoint}/${this.primaryKey}`);
+
         const deleteEvent = ModelEvent.delete(this);
         this.dispatchEvent(deleteEvent);
         this.constructor.dispatchEvent(deleteEvent);
     }
 
     assign(data = {}) {
-        Object.entries(this.constructor.fields).forEach(([field, { type }]) => {
-            if (field in data) {
-                this[field] = this.constructor.castFieldValue(data[field], type);
-            }
+        Object.keys(this.constructor.fields).forEach(field => {
+            if (field in data) this[field] = data[field];
         });
         const primaryKeyValue = data[this.constructor.primaryField];
-        if (primaryKeyValue && !this.constructor.instanceCache.has(primaryKeyValue)) {
-            this.constructor.instanceCache.set(primaryKeyValue, this);
+        if (primaryKeyValue && !this.constructor._instanceCache.has(primaryKeyValue)) {
+            this.constructor._instanceCache.set(primaryKeyValue, this);
             this.constructor.dispatchEvent(ModelEvent.new(this));
         }
         if (Object.keys(this.fieldChanges).length) {
@@ -82,19 +222,28 @@ class Model extends EventTarget {
         this.fieldChanges = {};
     }
 
+    get endpoint() {
+        if (!this.constructor.endpoint) return;
+        var endpoint = this.constructor.endpoint;
+        Object.keys(this.constructor.fields).forEach(field => {
+            endpoint = endpoint.replace(`:${field}`, String(this[field]));
+        });
+        return endpoint;
+    }
+
     get primaryKey() { return this[this.constructor.primaryField]; }
     static get primaryField() {
-        const primaryField = Object.keys(this.fields).find(field => this.fields[field].primary_key);
+        const primaryField = Object.keys(this.fields).find(field => this.fields[field].primaryKey);
         if (primaryField) return primaryField;
         if ('id' in this.fields) return 'id';
         throw new Error("Model must have a primary key or 'id' field defined.");
     }
 
-    static dispatchEvent(event) { this.classEventTarget.dispatchEvent(event); }
-    static addEventListener(type, callback) { this.classEventTarget.addEventListener(type, callback); }
-    static removeEventListener(type, callback) { this.classEventTarget.removeEventListener(type, callback); }
+    static dispatchEvent(event) { this._classEventTarget.dispatchEvent(event); }
+    static addEventListener(type, callback) { this._classEventTarget.addEventListener(type, callback); }
+    static removeEventListener(type, callback) { this._classEventTarget.removeEventListener(type, callback); }
 
-    static castFieldValue(value, type) {
+    static _castFieldValue(value, type) {
         if (value === null || value === undefined) return value;
         switch (type) {
             case Date:
@@ -110,14 +259,13 @@ class Model extends EventTarget {
 
     toJSON() {
         const jsonObject = {};
-        Object.keys(this.constructor.fields).forEach((key) => {
-            const value = this[key];
-            if (value !== null && value !== undefined) jsonObject[key] = value instanceof Date ? value.toISOString() : value;
+        Object.keys(this.constructor.fields).forEach(field => {
+            const value = this[field];
+            if (value !== null && value !== undefined) jsonObject[field] = value instanceof Date ? value.toISOString() : value;
         });
         return jsonObject;
     }
 }
-
 
 class ModelEvent extends CustomEvent {
     static NEW = 'new';
@@ -134,11 +282,6 @@ class ModelEvent extends CustomEvent {
     static delete(target) { return new ModelEvent(ModelEvent.DELETE, target); }
     static change(target, delta) { return new ModelEvent(ModelEvent.CHANGE, { target, delta }); }
 }
-
-
-
-
-
 
 class Templater {
     static clone(template) {
@@ -167,9 +310,13 @@ class Templater {
                     const targetElements = resolveElements(beforeEvent);
                     if (targetElements.length > 0) {
                         targetElements.forEach(targetElement => {
-                            const elementForCallback = resolveElements(fullSelector)[0] || targetElement;
-                            targetElement.removeEventListener(eventType, (event) => callback(elementForCallback, event));
-                            targetElement.addEventListener(eventType, (event) => callback(elementForCallback, event));
+                            // dont reapply event listeners
+                            if (!targetElement._eventListeners) targetElement._eventListeners = new Set();
+                            if (!targetElement._eventListeners.has(rawSelector)) {
+                                const elementForCallback = resolveElements(fullSelector)[0] || targetElement;
+                                targetElement.addEventListener(eventType, (event) => callback(elementForCallback, event));
+                                targetElement._eventListeners.add(rawSelector);
+                            }
                         });
                     } else console.warn(`Templater: Event target element(s) not found for base selector: "${beforeEvent}"`);
                 } else {
@@ -185,33 +332,41 @@ class Templater {
 
 class Router {
     static routes = [];
-    static currentRoute = null;
+    static defaultPath = '/';
 
     static route(path, callback) { this.routes.push({ path, callback }); }
 
-    static navigate(path) {
-        const route = this.matchRoute(path || window.location.pathname);
-        if (!route) this.go('/', replace = true);
+    static go(path, replace = false) {
+        if (replace) history.replaceState({}, '', path);
+        else history.pushState({}, '', path);
+        this.#navigate(path);
+    }
 
-        this.currentRoute = route;
+    static init() {
+        this.#navigate();
+        window.addEventListener('popstate', () => this.#navigate());
+    }
+
+    static #navigate(path) {
+        const route = this.#matchRoute(path || window.location.pathname);
+        if (!route) return this.go(this.defaultPath, true);
         route.callback(route.params, route.queryParams);
     }
 
-    static matchRoute(path) {
+    static #matchRoute(path) {
         for (const route of this.routes) {
-            const match = this.matchPath(path, route.path);
+            const match = this.#matchPath(path, route.path);
             if (match) return { ...route, params: match.params, queryParams: match.queryParams };
         }
         return null;
     }
 
-    static matchPath(path, routePattern) {
+    static #matchPath(path, routePattern) {
         const [urlPath, queryString] = path.split('?');
         const queryParams = new URLSearchParams(queryString);
 
         const pathParts = urlPath.split('/').filter(Boolean);
         const patternParts = routePattern.split('/').filter(Boolean);
-
         if (pathParts.length !== patternParts.length) return null;
 
         const params = {};
@@ -223,17 +378,9 @@ class Router {
                 return null;
             }
         }
-
         return { params, queryParams };
     }
-
-    static go(path, replace = false) {
-        if (replace) history.replaceState({}, '', path);
-        else history.pushState({}, '', path);
-        this.navigate(path);
-    }
 }
-window.addEventListener('popstate', () => Router.navigate(location.pathname));
 
 class API {
     static baseUrl = "/api";
