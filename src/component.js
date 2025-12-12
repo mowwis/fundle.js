a64d4d21861287b281c6624703083179521d7c87
mowwis <mo.sph@rosphere.com>
2025-12-12 23:51:39 +0100

    changed variabes to private

--- src/component.js
+++ src/component.js
@@ -6,35 +6,38 @@ export class Component extends HTMLElement {
     static template = null;
 
     // Model
+    #model;
     #onModelChange = () => this.render();
     #onModelDelete = () => this.remove();
-    get model() { return this._model; }
+    get model() { return this.#model; }
     set model(modelInstance) {
-        if (this._model) {
-            this._model.removeEventListener(ModelEvent.CHANGE, this.#onModelChange);
-            this._model.removeEventListener(ModelEvent.DELETE, this.#onModelDelete);
+        if (this.#model) {
+            this.#model.removeEventListener(ModelEvent.CHANGE, this.#onModelChange);
+            this.#model.removeEventListener(ModelEvent.DELETE, this.#onModelDelete);
         }
         if (!modelInstance instanceof Model) throw new Error(`The model must be an instance of Model.`);
-        this._model = modelInstance;
-        if (this._model) {
-            this._model.addEventListener(ModelEvent.CHANGE, this.#onModelChange);
-            this._model.addEventListener(ModelEvent.DELETE, this.#onModelDelete);
+        this.#model = modelInstance;
+        if (this.#model) {
+            this.#model.addEventListener(ModelEvent.CHANGE, this.#onModelChange);
+            this.#model.addEventListener(ModelEvent.DELETE, this.#onModelDelete);
         }
     }
 
     // Collection
+    #collection;
     #onCollectionChange = () => this.render();
-    get collection() { return this._collection; }
+    get collection() { return this.#collection; }
     set collection(collectionInstance) {
-        if (this._collection) {
-            this._collection.removeEventListener(ModelEvent.NEW, this.#onCollectionChange);
-            this._collection.removeEventListener(ModelEvent.CHANGE, this.#onCollectionChange);
+        if (this.#collection) {
+            this.#collection.removeEventListener(ModelEvent.NEW, this.#onCollectionChange);
+            this.#collection.removeEventListener(ModelEvent.CHANGE, this.#onCollectionChange);
         }
         if (!collectionInstance instanceof Collection) throw new Error(`The collection must be an instance of Collection.`);
-        this._collection = collectionInstance;
-        if (this._collection) {
-            this._collection.addEventListener(ModelEvent.NEW, this.#onCollectionChange);
-            this._collection.addEventListener(ModelEvent.CHANGE, this.#onCollectionChange);
+        this.#collection = collectionInstance;
+
+        if (this.#collection) {
+            this.#collection.addEventListener(ModelEvent.NEW, this.#onCollectionChange);
+            this.#collection.addEventListener(ModelEvent.CHANGE, this.#onCollectionChange);
         }
     }
 
